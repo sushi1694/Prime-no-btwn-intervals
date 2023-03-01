fa30b46b0cba33fb6e476ec0044aba96c2602bd0
sushi1694 <124113986+sushi1694@users.noreply.github.com>
2023-03-01 12:42:11 +0530

    Create m&n.c

---
+++ m&n.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n1,n2,i,j,flag;
+    //Enter the interval numbers..
+    scanf("%d %d",&n1,&n2);
+    
+    for(i=n1+1 ; i<n2; i++)
+    {
+    	flag=0;
+        for(j=2; j<=i/2; j++)
+        {
+            if(i%j==0)
+            {
+                flag=1;
+            }
+        }
+        if(flag == 0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
